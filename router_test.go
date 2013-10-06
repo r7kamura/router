75bbbecc3cf8605774ce53b1d61eb676555190db
Ryo Nakamura <r7kamura@gmail.com>
2013-10-06 21:11:06 +0900

    gospel's interface was changed

--- router_test.go
+++ router_test.go
@@ -33,7 +33,7 @@ func TestRoute(t *testing.T) {
 		{"/a/:b/c", "/a/b/c", true},
 		{"/a/:b/c", "/a/b/c/d", false},
 	}
-	Describe(t, "router.Route#Match", func(context Context, it It) {
+	Describe(t, "router.Route#Match", func() {
 		for _, example := range examples {
 			var verb string
 			if example.match {
@@ -41,8 +41,8 @@ func TestRoute(t *testing.T) {
 			} else {
 				verb = " does not match "
 			}
-			it(example.pattern + verb + example.path, func(expect Expect) {
-				expect(NewRoute(example.pattern, dummyHandler).Match(example.path)).ToEqual(example.match)
+			It(example.pattern + verb + example.path, func() {
+				Expect(NewRoute(example.pattern, dummyHandler).Match(example.path)).To(Equal, example.match)
 			})
 		}
 	})
@@ -72,100 +72,100 @@ var dummyHandler http.Handler = http.HandlerFunc(func(writer http.ResponseWriter
 })
 
 func TestRouter(t *testing.T) {
-	Describe(t, "router.Router", func(context Context, it It) {
+	Describe(t, "router.Router", func() {
 		router := NewRouter()
 		router.Get("/a", dummyHandler)
 		router.Get("/:any", dummyHandler)
 		router.Get("/b", dummyHandler)
 		router.Any("/c/d", dummyHandler)
 
-		context("with unrelated path request", func() {
-			it("does not match & return default 404 response", func(expect Expect) {
+		Context("with unrelated path request", func() {
+			It("does not match & return default 404 response", func() {
 				response := get(router, "/a/b/c")
-				expect(response.Code).ToEqual(404)
-				expect(response.Body.String()).ToEqual("Not Found\n")
+				Expect(response.Code).To(Equal, 404)
+				Expect(response.Body.String()).To(Equal, "Not Found\n")
 			})
 		})
 
-		context("with custom 404 handler", func() {
+		Context("with custom 404 handler", func() {
 			customNotFoundRouter := NewRouter()
 			customNotFoundRouter.NotFoundHandler = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 				http.Error(writer, "Custom NotFoundHandler", 403)
 			})
-			it("returns custom response", func(expect Expect) {
-				expect(get(customNotFoundRouter, "/404").Code).ToEqual(403)
+			It("returns custom response", func() {
+				Expect(get(customNotFoundRouter, "/404").Code).To(Equal, 403)
 			})
 		})
 
-		context("with unrelated method request", func() {
-			it("does not match", func(expect Expect) {
-				expect(post(router, "/a").Code).ToEqual(404)
+		Context("with unrelated method request", func() {
+			It("does not match", func() {
+				Expect(post(router, "/a").Code).To(Equal, 404)
 			})
 		})
 
-		context("with related method request", func() {
-			it("does not match", func(expect Expect) {
-				expect(get(router, "/a").Code).ToEqual(200)
+		Context("with related method request", func() {
+			It("does not match", func() {
+				Expect(get(router, "/a").Code).To(Equal, 200)
 			})
 		})
 
-		context("with 2 related pattern", func() {
-			it("matches the first", func(expect Expect) {
-				expect(get(router, "/a").Body.String()).ToEqual("/a?")
+		Context("with 2 related pattern", func() {
+			It("matches the first", func() {
+				Expect(get(router, "/a").Body.String()).To(Equal, "/a?")
 			})
 		})
 
-		context("with path params pattern", func() {
-			it("addes query string with path params", func(expect Expect) {
-				expect(get(router, "/c").Body.String()).ToEqual("/c?any=c")
+		Context("with path params pattern", func() {
+			It("addes query string with path params", func() {
+				Expect(get(router, "/c").Body.String()).To(Equal, "/c?any=c")
 			})
 		})
 
-		context("with duplicated params", func() {
-			it("merged query string with path params", func(expect Expect) {
-				expect(get(router, "/c?any=d").Body.String()).ToEqual("/c?any=d&any=c")
+		Context("with duplicated params", func() {
+			It("merged query string with path params", func() {
+				Expect(get(router, "/c?any=d").Body.String()).To(Equal, "/c?any=d&any=c")
 			})
 		})
 
-		context("with Any route", func() {
-			it("matches any method", func(expect Expect) {
-				expect(get(router, "/c/d").Code).ToEqual(200)
-				expect(post(router, "/c/d").Code).ToEqual(200)
+		Context("with Any route", func() {
+			It("matches any method", func() {
+				Expect(get(router, "/c/d").Code).To(Equal, 200)
+				Expect(post(router, "/c/d").Code).To(Equal, 200)
 			})
 		})
 
-		context("with host route", func() {
+		Context("with host route", func() {
 			hostRouter := NewRouter()
 			hostRouter.Host("example.com")
 			hostRouter.Get("/a", dummyHandler)
-			it("matches related host", func(expect Expect) {
-				expect(get(hostRouter, "/a").Code).ToEqual(404)
-				expect(get(hostRouter, "http://example.com/a").Code).ToEqual(200)
+			It("matches related host", func() {
+				Expect(get(hostRouter, "/a").Code).To(Equal, 404)
+				Expect(get(hostRouter, "http://example.com/a").Code).To(Equal, 200)
 			})
 		})
 
-		context("with many host routes", func() {
+		Context("with many host routes", func() {
 			mainRouter := NewRouter()
 			mainRouter.Get("/a", dummyHandler)
 			apiRouter := NewRouter()
 			apiRouter.Host("api.example.com")
 			apiRouter.Get("/b", dummyHandler)
 			apiRouter.NotFoundHandler = mainRouter
-			it("matches related host", func(expect Expect) {
-				expect(get(apiRouter, "/a").Code).ToEqual(200)
-				expect(get(apiRouter, "/b").Code).ToEqual(404)
-				expect(get(apiRouter, "http://api.example.com/a").Code).ToEqual(200)
-				expect(get(apiRouter, "http://api.example.com/b").Code).ToEqual(200)
+			It("matches related host", func() {
+				Expect(get(apiRouter, "/a").Code).To(Equal, 200)
+				Expect(get(apiRouter, "/b").Code).To(Equal, 404)
+				Expect(get(apiRouter, "http://api.example.com/a").Code).To(Equal, 200)
+				Expect(get(apiRouter, "http://api.example.com/b").Code).To(Equal, 200)
 			})
 		})
 
-		context("with Handle handler", func() {
+		Context("with Handle handler", func() {
 			anyRouter := NewRouter()
 			anyRouter.Handle(dummyHandler)
-			it("matches any request", func(expect Expect) {
-				expect(get(anyRouter, "/a").Code).ToEqual(200)
-				expect(get(anyRouter, "/b").Code).ToEqual(200)
-				expect(post(anyRouter, "/b").Code).ToEqual(200)
+			It("matches any request", func() {
+				Expect(get(anyRouter, "/a").Code).To(Equal, 200)
+				Expect(get(anyRouter, "/b").Code).To(Equal, 200)
+				Expect(post(anyRouter, "/b").Code).To(Equal, 200)
 			})
 		})
 	})
